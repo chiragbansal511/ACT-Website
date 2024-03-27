2c8dfe8a0954708a418442a8900331cab5dbbf52
Deepanshi Sharma <90607584+deecodess@users.noreply.github.com>
2024-03-27 15:19:46 +0530

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="appWrapper"> 
+      <h1>Coming soon!</h1>
+      <h3>Stay Tuned for something awesome.</h3>
+    </div>
+  )
+}
+
+export default App
